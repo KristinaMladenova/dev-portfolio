23cb5cad0f6398c5b3700e6811532d82ecfbc909
Kristina Mladenova <gglinv@proton.me>
2022-12-02 19:17:42 +0100

    made everything

---
+++ src/components/projects/dev-projects.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Project from "../shared/project";
+
+const DevProjects = () => {
+  return (
+    <Box>
+      <Project />
+      <Project />
+    </Box>
+  );
+};
+export default DevProjects;
---
+++ src/components/projects/uiux-projects.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Project from "../shared/project";
+
+const Uiux = () => {
+  return (
+    <Box>
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </Box>
+  );
+};
+export default Uiux;
